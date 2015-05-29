1680d20b175f55a1cd472f3d88dd1c4eff553410
jeremy <kepungjerwo>
2015-05-29 17:45:59 +0800

    Initial commit.

---
+++ MovieAzure/MovieAzure/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MovieAzure.Startup))]
+namespace MovieAzure
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
